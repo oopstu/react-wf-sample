2da0f5d82bd1b0a2f04c932978325167c6c6d7f1
oopstu <heath@decisions.com>
2018-06-25 16:43:50 -0400

    More updates to React based example for zoodata.

--- src/App.tsx
+++ src/App.tsx
@@ -4,22 +4,14 @@ import TaskList from "./components/TaskList";
 import logo from "./logo.svg";
 
 
-
-const contacts = [
-  { id: 1, name: "Leanne Graham" },
-  { id: 2, name: "Ervin Howell" },
-  { id: 3, name: "Clementine Bauch" },
-  { id: 4, name: "Patricia Lebsack" }
-];
-
 class App extends React.Component {
 
-  private sessionId : string;
+  // private sessionId : string;
 
   public componentDidMount() {
     // // tslint:disable-next-line
     // debugger;
-    this.GetSessionAndGetTasks();
+    // this.GetSessionAndGetTasks();
   }
 
   public render() {
@@ -30,40 +22,14 @@ class App extends React.Component {
           <h1 className="App-title">React Based Workflow Task Example</h1>
         </header>
 
-        <TaskList tasks={contacts} />
+        {/* <TaskList tasks={contacts} /> */}
+        <TaskList />
       </div>
     );
   }
 
   
-  private GetSessionAndGetTasks() {
-    // Start the fetch!
-    fetch("http://localhost/decisions/Primary/REST/AccountService/LoginUser?userid=admin@decisions.com&password=admin&outputType=JSON", {
-      // headers:{
-      //   "Access-Control-Allow-Headers": "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers",
-      //   "Access-Control-Allow-Methods": "GET,HEAD,OPTIONS,POST,PUT",
-      //   'Access-Control-Allow-Origin':'*',
-      //   }, 
-      // mode: "cors",
-    }).then(response => {
-      return response.json();
-    }).then(LoginUserResult => {
-      this.sessionId = this.GetSessionIdFromResult(LoginUserResult);
-      // tslint:disable-next-line
-      console.log(this.sessionId);
-    }).catch(err => {
-        // tslint:disable-next-line
-        console.log(err);
-      });
-  }
-
-  private GetSessionIdFromResult(LoginUserResult: any) : string {
-    // // tslint:disable-next-line
-    // console.log(LoginUserResult);
-    return LoginUserResult.SessionValue;
-    
-  }
-
+  
  
 }
 
--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,14 +1,96 @@
 
 import * as React from "react";
+// import the Task component
+import './Task.css'
 
-// import the Contact component
-import Task from "./Task";
+interface ITaskState { tasks: any[] };
+class TaskList extends React.Component<{}, ITaskState> {
 
-function TaskList(props:any) {
-  return (
-    <div>{props.tasks.map((c: any) => <Task key={c.id} name={c.name} />)}</div>
-  );
+  private sessionId: string;
+
+  constructor(props: any) {
+
+    super(props);
+    this.state = {
+      tasks: [],
+    }
+  }
+
+  public componentDidMount() {
+    // // tslint:disable-next-lines
+    // debugger;
+    this.GetSessionAndGetTasks();
+  }
+
+  public render() {
+    const { tasks } = this.state;
+
+    return (
+      <div className="task-list">
+        
+          {tasks.map((c: any) => 
+            <div className="internal-tasks" key={c.AssignmentId}><span className="task">Item: {c.EntityName}</span></div>
+          )}
+        
+        <div className="task-count"><span>Task Count: {this.state.tasks.length}</span></div>
+      </div>
+    );
+  }
+
+  private GetSessionAndGetTasks() {
+    // Start the fetch!
+    fetch("http://localhost/decisions/Primary/REST/AccountService/LoginUser?userid=admin@decisions.com&password=admin&outputType=JSON", {
+      // headers:{
+      //   "Access-Control-Allow-Headers": "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers",
+      //   "Access-Control-Allow-Methods": "GET,HEAD,OPTIONS,POST,PUT",
+      //   'Access-Control-Allow-Origin':'*',
+      //   }, 
+      // mode: "cors",
+    }).then(response => {
+      return response.json();
+    }).then(LoginUserResult => {
+      // tslint:disable-next-line
+      // console.log("Getting session id from result: " + JSON.stringify(LoginUserResult));
+      this.sessionId = this.GetSessionIdFromResult(LoginUserResult);
+      // tslint:disable-next-line
+      console.log(this.sessionId);
+      this.GetTasksWithSessionId();
+
+    }).catch(err => {
+      // tslint:disable-next-line
+      console.log(err);
+    });
+  }
+
+  private GetSessionIdFromResult(ResponseData: any): string {
+    // // tslint:disable-next-line
+    // console.log(LoginUserResult);
+    return ResponseData.LoginUserResult.SessionValue;
+
+  }
+
+  private GetTasksWithSessionId() {
+    fetch("http://localhost/decisions/Primary/REST/Assignment/GetMyCurrentAssignments?sessionId=" + this.sessionId + "&outputType=JSON", {
+      // headers:{
+      //   "Access-Control-Allow-Headers": "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers",
+      //   "Access-Control-Allow-Methods": "GET,HEAD,OPTIONS,POST,PUT",
+      //   'Access-Control-Allow-Origin':'*',
+      //   }, 
+      // mode: "cors",
+    }).then(response => {
+      return response.json();
+    }).then(CurrentTasksResult => {
+      // tslint:disable-next-line
+      console.log("CurrentTasks result: " + JSON.stringify(CurrentTasksResult.GetMyCurrentAssignmentsResult));
+      this.setState( { tasks: CurrentTasksResult.GetMyCurrentAssignmentsResult } );
+      // tslint:disable-next-line
+      console.log("State: " + JSON.stringify(this.state.tasks[0]));
+    }).catch(err => {
+      // tslint:disable-next-line
+      console.log(err);
+    });
+
+  }
+
 }
-
-
 export default TaskList;
