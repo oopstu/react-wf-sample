aad9d34fd140c466f6284de30d68d61258c32cbb
oopstu <heath@decisions.com>
2018-06-27 10:58:38 -0400

    oleg helping heath!

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,10 +1,10 @@
 
 import * as React from "react";
-import DecisionsIFrame from './DecisionsIFrame';
 // import the Task component
 import './Task.css';
 
-interface ITaskState { tasks: any[], clicked: boolean, iFrameURL: string };
+interface ITaskState { tasks: any[], iFrameURL: string };
+
 class TaskList extends React.Component<{}, ITaskState> {
 
   private sessionId: string;
@@ -14,10 +14,9 @@ class TaskList extends React.Component<{}, ITaskState> {
     super(props);
     this.handleBtnClick = this.handleBtnClick.bind(this);
     this.state = {
-      clicked: false,
       iFrameURL: "",
       tasks: []
-      
+
     }
   }
 
@@ -29,31 +28,48 @@ class TaskList extends React.Component<{}, ITaskState> {
 
   public render() {
     const { tasks } = this.state;
-    const clicked = this.state.clicked;
-    let button;
-
-    if (clicked) {
-       button = <DecisionsIFrame  />;
-    }
 
     return (
       <div className="task-list">
-          
-          {tasks.map((c: any) => 
+
+        {tasks.map((task: any) =>
+          // tslint:disable-next-line jsx-no-lambda
+          <div className="internal-tasks"
+            key={task.AssignmentId}
             // tslint:disable-next-line jsx-no-lambda
-            <div className="internal-tasks" 
-              key={c.AssignmentId} 
-              onClick={ e => this.handleBtnClick(e) }>
-                  Item: {c.EntityName} (Created by: {c.CreatedBy} on {c.CreatedOnDate})
-            </div>            
-          )}
-        
-        <div className="task-count"><span>Task Count: {this.state.tasks.length}</span></div>    
+            onClick={e => this.handleBtnClick(e, task.AssignmentId)}>
+            Item: {task.EntityName} (Created by: {task.CreatedBy} on {task.CreatedOnDate})
+            </div>
+        )}
 
-        {button}
+        <div className="task-count"><span>Task Count: {this.state.tasks.length}</span></div>
 
-		  </div>
+        {
+          // tslint:disable-next-line jsx-no-lambda
+          (this.state.iFrameURL.length || this.state.iFrameURL.length > 1) ? <iframe src={this.state.iFrameURL} onLoad={ev => this.doMyFrameLoad} className={"task-present"} /> : ''
+        }
+      </div>
     );
+  }
+
+  private doMyFrameLoad(frame: any) {
+    // This is the really important bit
+    // that detects when the flow engine sends 
+    // a flowExecutionCompleted event without any 
+    // special end step at all.
+    frame.contentWindow.$("div[id^='formWrapper']").on("flowExecutionCompleted", () => {
+      // alert("Flow completed!!");
+      // document.getElementById('iframeworkflowchild').style.display = 'none';
+      this.setState( { iFrameURL: "" } );
+    });
+
+    // When second form in set is different size...
+    // frame needs to resize at that moment.
+    frame.contentWindow.$(frame.contentWindow.document).on("FormLoadComplete",  (resizeObj: any) => {
+      if (false === resizeObj.isResized) {
+        alert("height:" + resizeObj.FormSurfaceElem.height() + " width:" + resizeObj.FormSurfaceElem.width())
+      }
+    });
   }
 
   private GetSessionAndGetTasks() {
@@ -89,33 +105,29 @@ class TaskList extends React.Component<{}, ITaskState> {
   }
 
   private GetTasksWithSessionId() {
-    fetch("http://localhost/decisions/Primary/REST/Assignment/GetMyCurrentAssignments?sessionId=" + this.sessionId + "&outputType=JSON", {
-      // headers:{
-      //   "Access-Control-Allow-Headers": "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers",
-      //   "Access-Control-Allow-Methods": "GET,HEAD,OPTIONS,POST,PUT",
-      //   'Access-Control-Allow-Origin':'*',
-      //   }, 
-      // mode: "cors",
-    }).then(response => {
-      return response.json();
-    }).then(CurrentTasksResult => {
-      // tslint:disable-next-line
-      console.log("CurrentTasks result: " + JSON.stringify(CurrentTasksResult.GetMyCurrentAssignmentsResult));
-      this.setState( { tasks: CurrentTasksResult.GetMyCurrentAssignmentsResult } );
-      // tslint:disable-next-line
-      console.log("State: " + JSON.stringify(this.state.tasks[0]));
-    }).catch(err => {
-      // tslint:disable-next-line
-      console.log(err);
-    });
+    // 
+    fetch("http://localhost/decisions/Primary/REST/Assignment/GetMyCurrentAssignments?sessionId=" + this.sessionId + "&outputType=JSON")
+      .then(response => {
+        return response.json();
+      }).then(CurrentTasksResult => {
+        // tslint:disable-next-line
+        console.log("CurrentTasks result: " + JSON.stringify(CurrentTasksResult.GetMyCurrentAssignmentsResult));
+        this.setState({ tasks: CurrentTasksResult.GetMyCurrentAssignmentsResult });
+        // tslint:disable-next-line
+        console.log("State: " + JSON.stringify(this.state.tasks[0]));
+      }).catch(err => {
+        // tslint:disable-next-line
+        console.log(err);
+      });
 
   }
 
-  private handleBtnClick(event: any) : void {	 
-      // tslint:disable-next-line
-      
-    this.setState( { clicked: !this.state.clicked, iFrameURL: "" } );
+  private handleBtnClick(event: any, taskId: string): void {
+    // tslint:disable-next-line
+
+    const url = `http://localhost/decisions/?assignmentId=${taskId}&sessionid=${this.sessionId}&chrome=off&border=true`
+    this.setState({ iFrameURL: url });
   }
- 
+
 }
 export default TaskList;
