e021ed2372975aa5bb04d03dfe1166996e0bfc04
oleg <olegraf@gmail.com>
2018-06-28 02:26:47 +0300

    dialog for a task view

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,9 +1,19 @@
 
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogTitle from '@material-ui/core/DialogTitle';
+
 import * as React from "react";
 import Task from './Task';
 import './Task.css';
 
-interface ITaskState { tasks: any[], iFrameURL: string };
+interface ITaskState {
+  isDialogOpen: boolean
+  iFrameURL: string,
+  tasks: any[],
+};
 
 class TaskList extends React.Component<{}, ITaskState> {
 
@@ -12,11 +22,14 @@ class TaskList extends React.Component<{}, ITaskState> {
   constructor(props: any) {
 
     super(props);
-    
+
     this.state = {
       iFrameURL: "",
-      tasks: []
+      isDialogOpen: false,
+      tasks: [],
     }
+
+    this.handleDialogClose.bind(this);
   }
 
   public componentDidMount() {
@@ -34,34 +47,53 @@ class TaskList extends React.Component<{}, ITaskState> {
         {tasks.map((task: any) =>
           // tslint:disable-next-line jsx-no-lambda
           <Task key={task.AssignmentId} id={task.AssignmentId} name={task.EntityName} onClick={() => this.handleBtnClick(task.id)}
-          createdBy={task.CreatedBy} createdOnDate={task.CreatedOnDate}
-          priority={task.Priority} />
+            createdBy={task.CreatedBy} createdOnDate={task.CreatedOnDate}
+            priority={task.Priority} />
         )}
 
         <div className="task-count"><span>Task Count: {this.state.tasks.length}</span></div>
 
-        {
-          // tslint:disable-next-line jsx-no-lambda
-          (this.state.iFrameURL.length || this.state.iFrameURL.length > 1) ? 
-            <iframe src={this.state.iFrameURL} onLoad={this.doMyFrameLoad} className={"task-present"} /> : ''
-        }
+        <Dialog
+          open={this.state.isDialogOpen}
+          onClose={this.handleDialogClose}
+          aria-labelledby="form-dialog-title"
+        >
+          <DialogTitle id="form-dialog-title">Task Page</DialogTitle>
+          <DialogContent>
+            {
+              // tslint:disable-next-line jsx-no-lambda
+              (this.state.iFrameURL.length || this.state.iFrameURL.length > 1) ?
+                <iframe src={this.state.iFrameURL} onLoad={this.doMyFrameLoad} className={"task-present"} /> : ''
+            }
+          </DialogContent>
+          <DialogActions>              
+            <Button onClick={this.handleDialogClose} color="primary">
+              Close
+            </Button>
+          </DialogActions>
+        </Dialog>
       </div>
     );
   }
 
+  private handleDialogClose = () => {
+    this.setState({ isDialogOpen: false });
+  };
+
+
   private doMyFrameLoad(event: any) {
-        // This is the really important bit
+    // This is the really important bit
     // that detects when the flow engine sends 
     // a flowExecutionCompleted event without any 
     // special end step at all.
-    const frame = event.currentTarget;
-    // Heath, here is the cross-origin error appears...  
-    // Uncaught DOMException: Blocked a frame with origin "http://localhost:3000" from accessing a cross-origin frame.
-    frame.contentWindow.$("div[id^='formWrapper']").on("flowExecutionCompleted", () => {
-      // alert("Flow completed!!");
-      // document.getElementById('iframeworkflowchild').style.display = 'none';
-      this.setState( { iFrameURL: "" } );
-    });
+    // const frame = event.currentTarget;
+    // TODO: here is the cross-origin error appears...  
+    // // Uncaught DOMException: Blocked a frame with origin "http://localhost:3000" from accessing a cross-origin frame.
+    // frame.contentWindow.$("div[id^='formWrapper']").on("flowExecutionCompleted", () => {
+    //   // alert("Flow completed!!");
+    //   // document.getElementById('iframeworkflowchild').style.display = 'none';
+    //   this.setState({ iFrameURL: "" });
+    // });
 
     // When second form in set is different size...
     // frame needs to resize at that moment.
@@ -126,8 +158,13 @@ class TaskList extends React.Component<{}, ITaskState> {
     // tslint:disable-next-line
 
     const url = `http://localhost/decisions/?assignmentId=${taskId}&sessionid=${this.sessionId}&chrome=off&border=true`
-    this.setState({ iFrameURL: url });
+    this.setState({
+      iFrameURL: url,
+      isDialogOpen: true
+    });
   }
 
 }
+
+
 export default TaskList;
